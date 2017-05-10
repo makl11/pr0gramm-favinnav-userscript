6f53ab23732c768cc41147deee32b9e9111bd73a
makl11 <makl11@users.noreply.github.com>
2017-05-10 03:22:40 +0200

    upload

---
+++ pr0-fav-in-nav.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         pr0-fav-in-nav
+// @namespace    http://pr0gramm.com/user/playmax
+// @version      1.0
+// @description  Adds a heart-shaped link to the users favorites in the navigation bar
+// @author       playmax
+// @match        http://pr0gramm.com/*
+// @match        https://pr0gramm.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+jQuery(function() {
+  var data = jQuery.parseJSON(decodeURI($.cookie('me')));
+  var link = jQuery("<a id='tab-fav' class='head-tab' href='/user/" + data.n + "/likes'>❤</a>");
+  jQuery("#filter-link").before(link);
+});
